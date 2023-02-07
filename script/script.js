60e6c3eb66c0208dc2fb12eeb34540fb14025c3d
Alessandro Montenero <alemonte99@icloud.com>
2023-02-07 12:06:43 +0100

    base html-css-js

---
+++ script/script.js
@@ -0,0 +1,14 @@
+/**
+* Project Name
+* @author   Alessandro Montenero
+* @description 
+*/
+
+/**
+ * @name functionName
+ * @description functionDescription
+ * 
+ * @param {paramType} paramName - comment
+ * @returns {returnType} returnName
+ * @throws {errorType} errorName
+ */
